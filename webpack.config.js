fc3c76d1e87292720ed1f21fad8e3fafea78b74f
Anastar12 <ann.star7@yandex.ru>
2025-06-09 08:59:43 +0300

    change configs

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/StellarBurgers/'
   }
 };
